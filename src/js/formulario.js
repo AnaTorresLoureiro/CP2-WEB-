38e0f9009cc93a6c33cd93085d687258ed98b097
Lana <eduardinhoggsouza@gmail.com>
2024-05-03 20:50:42 -0300

    contato

---
+++ src/js/formulario.js
@@ -0,0 +1,10 @@
+let enviar = document.querySelector("#form")
+enviar.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let nome = document.querySelector(".name").value;
+    let email = document.querySelector(".email").value;
+    let celular = parseInt(document.querySelector(".celular").value);
+
+    console.log(nome, email, celular)
+})
